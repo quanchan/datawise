d991ef98ddfbe9edebaadd97ca5c4f4662da14a3
quanchan <quan.trananh12@gmail.com>
2023-10-24 14:42:43 +1100

    Window start script

--- start.ps1
+++ start.ps1
@@ -1,14 +1,5 @@
-# Run 'yarn'
-Start-Process -FilePath "yarn" -Wait
+# Open a new PowerShell window and run 'docker compose up' without waiting
+Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -NoExit -Command `"docker compose up`""
 
-# Open a new PowerShell window and run 'docker-compose up'
-Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -NoExit -Command "docker compose up""
-
-# Run 'yarn db'
-Start-Process -FilePath "yarn" -ArgumentList "db" -Wait
-
-# Run 'yarn build'
-Start-Process -FilePath "yarn" -ArgumentList "build" -Wait
-
-# Run 'yarn start' (use -NoNewWindow to keep the log open)
-Start-Process -FilePath "yarn" -ArgumentList "start" -NoNewWindow
+# Open a new PowerShell window and run 'yarn start' without waiting
+Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -NoExit -Command `"yarn start`""
---
+++ updateAndStart.ps1
@@ -0,0 +1,17 @@
+# Pull new changes from git
+Start-Process -FilePath "git" -ArgumentList "pull" -Wait
+
+# Run 'yarn'
+Start-Process -FilePath "yarn" -Wait
+
+# Open a new PowerShell window and run 'docker compose up' without waiting
+Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -NoExit -Command `"docker compose up`""
+
+# Run 'yarn db'
+Start-Process -FilePath "yarn" -ArgumentList "db" -Wait -NoNewWindow
+
+# Run 'yarn build'
+Start-Process -FilePath "yarn" -ArgumentList "build" -Wait -NoNewWindow
+
+# Open a new PowerShell window and run 'yarn start' without waiting
+Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -NoExit -Command `"yarn start`""
